608f5a3c4f4d4930dad046b130b29e55cb0de597
Robo Gombár <robo.gombar@gmail.com>
2025-06-13 11:44:23 +0000

    ideme na poslednu node kapitolu

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,19 +3,30 @@ from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD_TEXT)
-        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertEqual(node, node2)
 
-    def test_not_eq(self):
-        node3 = TextNode('This is a text node', TextType.BOLD_TEXT)
-        node4 = TextNode('This another text node', TextType.BOLD_TEXT)
-        self.assertNotEqual(node3, node4)
-    
-    def test_not_text_type(self):
-        node5 = TextNode('This is a text node', TextType.BOLD_TEXT)
-        node6 = TextNode('This is a text node', TextType.ITALIC_TEXT)
-        self.assertNotEqual(node5, node6)
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
 if __name__ == "__main__":
     unittest.main()
